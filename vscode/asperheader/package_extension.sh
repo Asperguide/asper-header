53211f2d0b5b04d73c5fb5d2625ea09b076d3686
Henry LETELLIER <henry.letellier@epitech.eu>
2025-09-28 12:00:44 +0200

    [ADD] a bash script in charge of building the program locally

---
+++ vscode/asperheader/package_extension.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Packaging extension..."
+echo "Installing vsce locally"
+npm install --include=dev vsce
+if [ $STATUS -ne 0 ]; then
+    echo "Install step failed, se above for more details."
+    exit $STATUS
+fi
+echo "Copying license from the root of the repository to the vscode extension..."
+cp -vf ../../LICENSE ./LICENSE
+echo "Copying the images and demos from the root of the repository to the vscode extension..."
+cp -rvf ../../images/* ./images
+echo "Running pre-publishing commands..."
+npm run package
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+    echo "Pre-publishing step failed, se above for more details."
+    exit $STATUS
+fi
+echo "Running the intergrated vsce:package (if present)..."
+npm run vsce:package
+if [ $? -ne 0 ]; then
+    echo "Running vsce manually..."
+    npx vsce package
+    STATUS=$?
+    if [ $STATUS -ne 0 ]; then
+        echo "Packaging failed, see above for more details."
+        exit $STATUS
+    fi
+fi
